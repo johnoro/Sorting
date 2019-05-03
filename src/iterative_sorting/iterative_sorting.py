537cf42f664f86ae50a1608c373a01c4e7531ab0
johnoro <officialjohnorourke@gmail.com>
2019-05-03 11:00:06 -0400

    Implements timsort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,12 +1,12 @@
 def insertion_sort(arr):
-	for i in range(1, len(arr)):
-		temp = arr[i]
-		j = i
-		while j > 0 and temp < arr[j-1]:
-			arr[j] = arr[j-1]
-			j -= 1
-		arr[j] = temp
-	return arr
+  for i in range(1, len(arr)):
+    temp = arr[i]
+    j = i
+    while j > 0 and temp < arr[j-1]:
+      arr[j] = arr[j-1]
+      j -= 1
+    arr[j] = temp
+  return arr
 
 
 print(insertion_sort([1, 3, 6, 2, 13, 41, 7]))
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -15,7 +15,7 @@ def quick_sort(arr):
   return left + [arr[pivot]] + right
 
 
-print(quick_sort([1, 3, 6, 2, 13, 41, 7]))
+# print(quick_sort([1, 3, 6, 2, 13, 41, 7]))
 
 from collections import deque
 def merge(arrA, arrB):
@@ -43,7 +43,7 @@ def merge_sort(arr):
   return merge(merge_sort(arr[:mid]), merge_sort(arr[mid:]))
 
 
-print(merge_sort([1, 3, 6, 2, 13, 41, 7]))
+# print(merge_sort([1, 3, 6, 2, 13, 41, 7]))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
@@ -77,9 +77,47 @@ def merge_sort_in_place(arr,l=0,r=0):
     return arr
   return sort(0, len(arr))
 
-print(merge_sort([1, 3, 6, 2, 13, 41, 7]))
+
+# print(merge_sort([1, 3, 6, 2, 13, 41, 7]))
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort(arr):
+def insertion_sort(arr, left=0, right=None):
+  if right is None:
+    right = len(arr) - 1
+  for i in range(left+1, right+1):
+    temp = arr[i]
+    j = i
+    while j > left and temp < arr[j-1]:
+      arr[j] = arr[j-1]
+      j -= 1
+    arr[j] = temp
   return arr
+
+# print(insertion_sort([13, 14, 2, 3, 5, 1, 6]))
+
+def compute_min_run(n):
+  r = 0
+  while n > 63:
+    r |= (n & 1)
+    m >>= 1
+  return n+r
+
+def timsort(arr):
+  min_run = compute_min_run(len(arr))
+  for i in range(0, len(arr), min_run):
+    insertion_sort(arr, i, min(i+min_run-1, len(arr)-1))
+  
+  size = min_run
+  while size < len(arr):
+    double_size = 2*size
+    for left in range(0, len(arr), double_size):
+      mid = left+size-1
+      right = min(left+double_size-1, n-1)
+
+      merge_in_place(arr, left, mid, right)
+    size *= 2
+
+  return arr
+
+print(timsort([1,3,4,-1,5,21,6,0]))
